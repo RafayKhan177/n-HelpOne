0014a96cbd9c4379dd2232a244bcedcad9c76b2d
Rafay Khan <151770177+akrakadabra@users.noreply.github.com>
2023-11-24 14:02:07 +0000

    bug fixes

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -2,7 +2,7 @@ import { ContributionCard, EmailBanner, Faq } from "components/Index";
 
 export default function page() {
   return (
-    <section style={{ margin: "3rem", gap: "2rem" }}>
+    <section>
       <Faq /> <ContributionCard /> <EmailBanner />
     </section>
   );
