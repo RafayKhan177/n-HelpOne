c25e5089d6203f7966e450a6ae84c08bb311619b
rafay <abdulrafaykhan857@gmail.com>
2023-12-27 17:12:22 +0500

    New Update

--- app/Projects/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import ProjectFounderDetails from "components/ProjectFounderDetails";
-import React from "react";
-import { projectsData } from "static";
-
-export default function page() {
-  return <ProjectFounderDetails data={projectsData} />;
-}
