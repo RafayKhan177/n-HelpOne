4ad629a8e543391e2656c36e08f40fe9721263d8
rafay <abdulrafaykhan857@gmail.com>
2023-12-27 16:04:50 +0500

    New Update

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,124 +1,55 @@
 "use client";
 
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Image, Show } from "@chakra-ui/react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import { Box, Image, Show } from "@chakra-ui/react";
-import { Button } from "@mantine/core";
-// import Image from "next/image";
 
-export default function Hero() {
-  const img1 =
-    "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/DDAl-Bashir%20Welfare%20excels%20in%20disaster%20management%2C%20swiftly%20responding%20to%20crises%20with%20effective%20relief%20e.png";
-  const img3 =
-    "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/WhatsApp%20Image%202023-12-25%20at%2018.29.36_01839d04.jpg";
-  const img4 =
-    "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/desk%20c/1.png";
-  const imgP1 =
-    "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/Mobo1AL-BASHIR%20WEL%20ARE.png";
-  const imgP2 =
-    "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/mobo%20c/1.png";
-  const imgP3 =
-    "https://github.com/RafayKhan177/Raw-Images/blob/main/2.png?raw=true";
+const imageUrls = [
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/desk%20c/1.png",
+  "https://github.com/RafayKhan177/Raw-Images/blob/main/3.png?raw=true",
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/DDAl-Bashir%20Welfare%20excels%20in%20disaster%20management%2C%20swiftly%20responding%20to%20crises%20with%20effective%20relief%20e.png",
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/5.png",
+];
+
+const mobileImageUrls = [
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/Mobo1AL-BASHIR%20WEL%20ARE.png",
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/mobo%20c/1.png",
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/mobo%20c/2.png",
+  "https://raw.githubusercontent.com/RafayKhan177/Raw-Images/main/4.png",
+];
+
+const Hero = () => {
+  const renderCarouselItem = (url, h) => (
+    <div key={url}>
+      <Image
+        src={url}
+        alt="hero-image"
+        width={900}
+        height={1500}
+        style={{
+          width: "100vw",
+          height: h,
+          maxHeight: "80vh",
+        }}
+      />
+    </div>
+  );
 
   return (
     <>
       <Show breakpoint="(min-width: 1000px)">
-        <Carousel autoPlay={true} infiniteLoop={true}>
-          <div>
-            <Image
-              src={img4}
-              alt="hero-image"
-              width={900}
-              height={1500}
-              style={{
-                width: "100vw",
-                height: "90vh",
-                objectFit: "cover",
-                maxHeight: "70vh",
-              }}
-            />
-          </div>
-
-          <div>
-            <Image
-              src={img3}
-              alt="hero-image"
-              width={900}
-              height={1500}
-              style={{
-                width: "100vw",
-                height: "90vh",
-                objectFit: "cover",
-                maxHeight: "70vh",
-              }}
-            />
-          </div>
-          <div>
-            <Image
-              src={img1}
-              alt="hero-image"
-              width={900}
-              height={1500}
-              style={{
-                width: "100vw",
-                height: "90vh",
-                objectFit: "cover",
-                maxHeight: "70vh",
-              }}
-            />
-          </div>
+        <Carousel autoPlay infiniteLoop>
+          {imageUrls.map((url) => renderCarouselItem(url, "70vh"))}
         </Carousel>
       </Show>
 
       <Show breakpoint="(max-width: 1000px)">
-        <Carousel autoPlay={true} infiniteLoop={true}>
-          <div>
-            <Image
-              src={imgP1}
-              alt="hero-image"
-              width={900}
-              height={1500}
-              style={{
-                width: "100vw",
-                height: "auto",
-                // objectFit: "cover",
-                maxHeight: "70vh",
-              }}
-            />
-          </div>
-
-          <div>
-            <Image
-              src={imgP2}
-              alt="hero-image"
-              width={900}
-              height={1500}
-              style={{
-                width: "100vw",
-                height: "auto",
-                // objectFit: "cover",
-                maxHeight: "70vh",
-              }}
-            />
-          </div>
-          <div>
-            <Image
-              src={imgP3}
-              alt="hero-image"
-              width={900}
-              height={1500}
-              style={{
-                width: "100vw",
-                height: "auto",
-                // objectFit: "cover",
-                maxHeight: "70vh",
-              }}
-            />
-          </div>
+        <Carousel autoPlay infiniteLoop>
+          {mobileImageUrls.map((url) => renderCarouselItem(url, "50vh"))}
         </Carousel>
       </Show>
     </>
   );
-}
+};
+
+export default Hero;
